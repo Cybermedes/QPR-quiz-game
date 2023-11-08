c599d74c681ebc8dbae7a1f6f02e5d0a4ab80be8
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-11-07 22:57:32 -0300

    Criar metodo para escrever a pergunta e dar pontuação

--- quiz.py
+++ quiz.py
@@ -2,6 +2,7 @@ import random
 import tomllib
 from pathlib import Path
 from typing import Any
+from string import ascii_lowercase
 """Script para preparar as perguntas e respostas"""
 
 
@@ -15,8 +16,24 @@ def preparar_questoes(path: Path, num_questoes: int) -> list[str]:
     return random.sample(lista_questoes, k=num_questions)
 
 
-def fazer_pergunta(questao):
-    pass
+def fazer_pergunta(questao: dict[str, Any]) -> int:
+    """Lança a pergunta no terminal e espera o input do jogador.
+    Retorna 1 ponto se acertar e 0 se errar"""
+    questao_principal: str = questao["question"]
+    resposta_correta: str = questao["answer"]
+    alternativas: list = [[questao["answer"]]] + questao["alternatives"]
+    random.shuffle(alternativas)
+    alternativas_numeradas = dict(zip(ascii_lowercase, alternativas))
+    for letra, alternativa in alternativas_numeradas.items():
+        print(f"{letra}) {alternativa}")
+
+    resposta = input("Digite a resposta da alternativa correta: ")
+    if resposta == resposta_correta:
+        print("⭐ Resposta Correta! ⭐")
+        return 1
+    else:
+        print(f"A resposta é {resposta_correta!r}, não {resposta!r}")
+        return 0
 
 
 def rodar_quiz() -> None:
