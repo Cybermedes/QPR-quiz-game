deef746ca6458ec6c6962026c4d4152e5a36603b
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-11-07 21:35:30 -0300

    Cria metodo para ler e retornar as questoes do arquivo toml

--- quiz.py
+++ quiz.py
@@ -1,4 +1,24 @@
+import random
+import tomllib
+from pathlib import Path
 """Script para preparar as perguntas e respostas"""
+
+
+def preparar_questoes(path: Path, num_questoes: int = 5) -> list[str]:
+
+    # Le as questões do arquivo toml
+    with open(path, mode="rb") as toml_arquivo:
+        questoes = tomllib.load(toml_arquivo)
+    num_questions = min(num_questoes, len(questoes))
+
+    # Retorna as questões em uma lista aleatória
+    return random.sample(questoes, k=num_questions)
+
+
+def fazer_pergunta(questao):
+    pass
+
+
 def rodar_quiz() -> None:
 
     # Ler e selecionar as perguntas do arquivo com o banco de questões
