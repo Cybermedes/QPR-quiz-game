aac814b98d03ca12bc12e1a3f51cc027fbc3d94a
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-11-07 23:03:09 -0300

    Corrige tipo de dados e aplica variaveis não usadas

--- quiz.py
+++ quiz.py
@@ -3,15 +3,14 @@ import tomllib
 from pathlib import Path
 from typing import Any
 from string import ascii_lowercase
-"""Script para preparar as perguntas e respostas"""
 
 
-def preparar_questoes(path: Path, num_questoes: int) -> list[str]:
+def preparar_questoes(path: Path, num_questoes: int) -> list[dict[str, Any]]:
     """Le as questoes arquivo toml e as retorna em uma lista"""
     with open(path, mode="rb") as toml_arquivo:
         questoes: dict[str, Any] = tomllib.load(toml_arquivo)
     num_questions = min(num_questoes, len(questoes))
-    lista_questoes: list[str, Any] = questoes["questions"]
+    lista_questoes: list[dict[str, Any]] = questoes["questions"]
 
     return random.sample(lista_questoes, k=num_questions)
 
@@ -19,11 +18,13 @@ def preparar_questoes(path: Path, num_questoes: int) -> list[str]:
 def fazer_pergunta(questao: dict[str, Any]) -> int:
     """Lança a pergunta no terminal e espera o input do jogador.
     Retorna 1 ponto se acertar e 0 se errar"""
+
     questao_principal: str = questao["question"]
     resposta_correta: str = questao["answer"]
     alternativas: list = [[questao["answer"]]] + questao["alternatives"]
     random.shuffle(alternativas)
     alternativas_numeradas = dict(zip(ascii_lowercase, alternativas))
+    print(questao_principal)
     for letra, alternativa in alternativas_numeradas.items():
         print(f"{letra}) {alternativa}")
 
@@ -38,9 +39,10 @@ def fazer_pergunta(questao: dict[str, Any]) -> int:
 
 def rodar_quiz() -> None:
     questoes_path: Path = Path("questions.toml")
+    numero_perguntas: int = 5
 
     # Ler e selecionar as perguntas do arquivo com o banco de questões
-    questoes: list[str] = preparar_questoes(questoes_path)
+    questoes: list[dict[str, Any]] = preparar_questoes(questoes_path, numero_perguntas)
 
     # pontuação
     num_corretas: int = 0
