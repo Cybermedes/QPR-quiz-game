24fa470e413dcec3ded2535494372af6aab8f48d
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-11-30 16:36:17 -0300

    Modifica função para usar modulo mais atual

--- qpr_quiz_game/terminal.py
+++ qpr_quiz_game/terminal.py
@@ -1,6 +1,18 @@
-import os
+import sys
+import subprocess
 
 
-def limpar_terminal() -> int:
-    """Limpar o terminal através de commando. cls para Windows ou clear para Unix"""
-    return os.system("cls" if os.name in ("nt", "dos") else "clear")
+def limpar_terminal() -> None:
+    """Limpar o terminal através de commando dependendo do S.O."""
+    sistema_operacional: str = sys.platform
+
+    # win32 para sistemas Windows
+    # darwin para MacOS e cygwin para emuladores Linux no windows, como MINGW64
+    if sistema_operacional == "win32":
+        subprocess.run("cls", shell=True)
+    elif (
+        sistema_operacional == "darwin"
+        or sistema_operacional == "linux"
+        or sistema_operacional == "cygwin"
+    ):
+        subprocess.run("clear", shell=True)
